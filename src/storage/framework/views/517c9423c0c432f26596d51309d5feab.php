2fda4adbc0f958e26ffbaec741710b9cbad41536
luiscarneiro13 <carneiroluis2@gmail.com>
2025-10-07 20:39:30 -0400

    ...

--- src/storage/framework/views/517c9423c0c432f26596d51309d5feab.php
+++
@@ -1,6 +0,0 @@
-<li class="nav-item">
-    <a class="nav-link" data-widget="fullscreen" href="#" role="button">
-        <i class="fas fa-expand-arrows-alt"></i>
-    </a>
-</li>
-<?php /**PATH D:\laragon\www\reportefallas\src\resources\views/vendor/adminlte/partials/navbar/menu-item-fullscreen-widget.blade.php ENDPATH**/ ?>

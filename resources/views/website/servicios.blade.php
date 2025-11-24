7449c81898bf349f7f52f05655f300155a8d5e3c
luiscarneiro13 <carneiroluis2@gmail.com>
2025-11-24 10:20:22 -0400

    ....

--- resources/views/website/servicios.blade.php
+++ resources/views/website/servicios.blade.php
@@ -18,13 +18,15 @@
                         data-aos-delay="0"></div>
                     <div class="col-md-6 bg-light text">
                         <div class="p-4">
-                            <h2 class="h5 mb-3 text-black line-height-sm">Automative Manufacturing</h2>
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. In ipsum error
-                                perspiciatis odit ullam officia. Lorem ipsum dolor sit amet, consectetur
-                                adipisicing elit. Laudantium voluptatum vel natus, repellat at optio.</p>
-                            <p class="mb-0"><a href="#" class=""><small
+                            <h2 class="h5 mb-3 text-black line-height-sm">Soldadura</h2>
+                            <p>Se cuenta con camiones de soldadura certificados y totalmente equipados para ejecutar
+                                trabajos de soldadura de mantenimiento, reparación y construcción de tuberías,
+                                estructuras e instalaciones metálicas en general para la industria petrolera. Se incluye
+                                personal competente certificado para brindar la máxima eficiencia, seguridad y calidad
+                                donde y cuando se necesite.</p>
+                            {{-- <p class="mb-0"><a href="#" class=""><small
                                         class="text-uppercase font-weight-bold text-black">Read
-                                        More</small></a></p>
+                                        More</small></a></p> --}}
                         </div>
                     </div>
                 </div>
@@ -32,17 +34,19 @@
             <div class="col-lg-6">
                 <div class="row no-gutters align-items-stretch h-100 half-sm">
                     <div class="col-md-6 bg-image order-md-2 order-lg-1 bg-sm-height"
-                        style="background-image: url('website/images/img_1_colored.jpg');" data-aos="fade"
+                        style="background-image: url('website/images/Izamiento.JPG');" data-aos="fade"
                         data-aos-delay="200"></div>
                     <div class="col-md-6 bg-light text order-md-1 order-lg-2">
                         <div class="p-4">
-                            <h2 class="h5 mb-3 text-black line-height-sm">Mechanical Engineering</h2>
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. In ipsum error
-                                perspiciatis odit ullam officia. Lorem ipsum dolor sit amet, consectetur
-                                adipisicing elit. Aperiam, cupiditate officia recusandae velit sint totam.</p>
-                            <p class="mb-0"><a href="#" class=""><small
+                            <h2 class="h5 mb-3 text-black line-height-sm">Equipos de Izamiento</h2>
+                            <p>La empresa dispone de dos tipos de equipos de izamiento: -Montacargas para la carga de
+                                tuberías, tanques, herramientas, entre otros. Los cuales permiten el buen desempeño de
+                                las operaciones en los taladros de perforación y otras instalaciones. -Brazos
+                                Hidráulicos: Se cuenta con brazos con capacidades desde 8 hasta 20 Toneladas ideales
+                                para el izamiento de materiales, tuberías, válvulas, entre otros.</p>
+                            {{-- <p class="mb-0"><a href="#" class=""><small
                                         class="text-uppercase font-weight-bold text-black">Read
-                                        More</small></a></p>
+                                        More</small></a></p> --}}
                         </div>
                     </div>
                 </div>
@@ -51,17 +55,17 @@
             <div class="col-lg-6">
                 <div class="row no-gutters align-items-stretch h-100 half-sm">
                     <div class="col-md-6 bg-image order-md-1 order-lg-2 bg-sm-height"
-                        style="background-image: url('website/images/img_2_colored.jpg');" data-aos="fade"
+                        style="background-image: url('website/images/MaquinariaPesada.JPG');" data-aos="fade"
                         data-aos-delay="300"></div>
                     <div class="col-md-6 bg-light text order-md-2 order-lg-1">
                         <div class="p-4">
-                            <h2 class="h5 mb-3 text-black line-height-sm">Oil &amp; Gas Energy</h2>
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. In ipsum error
-                                perspiciatis odit ullam officia. Lorem ipsum dolor sit amet, consectetur
-                                adipisicing elit. Alias fugiat facilis quasi ratione ducimus ipsam!</p>
-                            <p class="mb-0"><a href="#" class=""><small
+                            <h2 class="h5 mb-3 text-black line-height-sm">Maquinaria pesada y movimiento de tierra</h2>
+                            <p>Nuestras maquinarias están disponibles en óptimas condiciones operativas, a disposición
+                                para proyectos de construcción, obras civiles, mantenimiento, saneamiento ambiental y
+                                todo tipo de actividades; garantizando eficiencia, seguridad y rendimiento.</p>
+                            {{-- <p class="mb-0"><a href="#" class=""><small
                                         class="text-uppercase font-weight-bold text-black">Read
-                                        More</small></a></p>
+                                        More</small></a></p> --}}
                         </div>
                     </div>
                 </div>
@@ -73,13 +77,14 @@
                         data-aos-delay="400"></div>
                     <div class="col-md-6 bg-light text order-md-1">
                         <div class="p-4">
-                            <h2 class="h5 mb-3 text-black line-height-sm">Industrial Construction</h2>
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. In ipsum error
-                                perspiciatis odit ullam officia. Lorem ipsum dolor sit amet, consectetur
-                                adipisicing elit. Laudantium ullam est alias minus, expedita autem.</p>
-                            <p class="mb-0"><a href="#" class=""><small
+                            <h2 class="h5 mb-3 text-black line-height-sm">Transporte de carga pesada</h2>
+                            <p>Ofrecemos Low-Boy y bateas 100% operativas para el transporte de materiales, insumos,
+                                herramientas, tuberías, válvulas entre otros, en todo el territorio nacional. Contamos
+                                con operadores capacitados y certificados que garantizan el cumplimiento de las
+                                normativas y regulaciones establecidas. </p>
+                            {{-- <p class="mb-0"><a href="#" class=""><small
                                         class="text-uppercase font-weight-bold text-black">Read
-                                        More</small></a></p>
+                                        More</small></a></p> --}}
                         </div>
                     </div>
                 </div>
